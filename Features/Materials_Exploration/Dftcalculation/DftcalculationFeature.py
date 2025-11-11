1840a73878d8d66f796b037f9c5978d2f3d87b85
atishdixit16 <atishdixit16@gmail.com>
2025-11-11 14:07:37 +0000

    Rename the codebase according to metadata.json

--- Features/Materials_Exploration/Dftcalculation/DftcalculationFeature.py
+++ Features/Materials_Exploration/Dftcalculation/DftcalculationFeature.py
@@ -4,7 +4,7 @@ from Information_Units.Databases.DatabaseFactory import database_factory
 from Information_Units.Predictors.PredictorFactory import predictor_factory
 
 
-class DFTCalculationFeature(BaseFeature):
+class DftcalculationFeature(BaseFeature):
     def __init__(self, logger=None):
         super().__init__("DFT Calculation", logger)
     
